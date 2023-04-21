e7d484dc35e839a931908e6bbb158d121ea00063
PretzelFairy <131439322+PretzelFairy@users.noreply.github.com>
2023-04-21 18:09:23 +1000

    Create FormField.vue

---
+++ FormField.vue
@@ -0,0 +1,23 @@
+<template>
+  <multiselect v-model="value" :options="options"></multiselect>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+export default {
+props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+components: { Multiselect },
+data () {
+  return {
+    options: [
+    { id: 1, label: "A" },
+    { id: 2, label: "B" },
+    { id: 3, label: "C" }
+  ],
+  selected: []
+  }
+}
+}
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
